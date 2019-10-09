c0e6ab82a78ef67d3b9a729974fd2eb181caab1a
吕财 <email>
2019-10-09 18:49:39 +0800

    添加了name属性

--- src/main/java/com/shigeng/controller/GitTestController.java
+++ src/main/java/com/shigeng/controller/GitTestController.java
@@ -1,4 +1,11 @@
 package com.shigeng.controller;
 
+import com.shigeng.service.GitTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class GitTestController {
+    @Autowired
+    private GitTestService gitTestService;
 }
--- src/main/java/com/shigeng/service/impl/GitTestServiceimpl.java
+++ src/main/java/com/shigeng/service/impl/GitTestServiceimpl.java
@@ -1,6 +1,11 @@
 package com.shigeng.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class GitTestServiceimpl {
     public static void main(String[] args) {
         System.out.println("abc");
+
     }
 }
