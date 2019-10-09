6ec05bf441c6d127d44976d14dc7ff3012265675
吕财 <email>
2019-10-09 19:59:36 +0800

    feature 提交

--- src/main/java/com/shigeng/service/impl/GitTestServiceimpl.java
+++ src/main/java/com/shigeng/service/impl/GitTestServiceimpl.java
@@ -7,5 +7,8 @@ public class GitTestServiceimpl {
     public static void main(String[] args) {
         System.out.println("abc");
         System.out.println("sadfadgadg");
+        System.out.println("master");
+        System.out.println("dev");
+        System.out.println("feature=1324");
     }
 }
